1d68c39daf12e3269b44b050309f728f64e17ec1
Julio Toboso <JulioToboso@gmail.com>
2025-06-16 12:24:10 +0200

    Update Map_of_Backgrounds.py

--- AtlasLusoris/Map_of_Backgrounds.py
+++ AtlasLusoris/Map_of_Backgrounds.py
@@ -16,3 +16,10 @@ backgrounds = [
 	"Soldier",
 	"Wayfarer",
 	]
+
+BACKGROUND_FEATURES = {
+    "Acolyte":      ("Shelter of the Faithful", "You and your companions can expect free healing and care at temples."),
+    "Criminal":     ("Criminal Contact", "You have a reliable and trustworthy contact in the criminal underworld."),
+    "Noble":        ("Position of Privilege", "People assume you have the right to be wherever you are."),
+    "Hermit":       ("Discovery", "You have made a unique and powerful discovery."),
+	}
